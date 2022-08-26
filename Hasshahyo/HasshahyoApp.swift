90704d73d6c42f93b761f062cec780e5ee6f5b0d
yuruhuwakumiai <yuruhuwakumiai@gmail.com>
2022-08-26 17:28:35 +0900

    最初

---
+++ Hasshahyo/HasshahyoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HasshahyoApp.swift
+//  Hasshahyo
+//
+//  Created by 橋元雄太郎 on 2022/08/26.
+//
+
+import SwiftUI
+
+@main
+struct HasshahyoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
