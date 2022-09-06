eb8d948fbfa392f5474f490c600f6b384f6a1f1d
yuruhuwakumiai <yuruhuwakumiai@gmail.com>
2022-09-06 16:43:11 +0900

    博多方面時間の構造体作成

--- Hasshahyo/Source/View/ContentView.swift
+++ Hasshahyo/Source/View/ContentView.swift
@@ -16,7 +16,39 @@ struct TimeTable: Identifiable {
 }
 
 struct HakataTimeData {
-
+    static let midori_two = 0...412
+    static let relayKamome_four = 412...451
+    static let midori_six = 451...478
+    static let relaykamome_eight = 478...499
+    static let midori_ten = 499...533
+    static let relaykamome_twelve = 533...546
+    static let midori_fourteen = 546...591
+    static let relaykamome_sixteen = 591...616
+    static let midori_eighteen = 616...651
+    static let relaykamome_twenty = 651...677
+    static let midori_HTB_twentytwo = 677...689
+    static let relaykamome_twentyfour = 689...736
+    static let midori_HTB_twentysix = 736...747
+    static let relaykamome_twentyeight = 747...795
+    static let midori_HTB_thirty = 795...809
+    static let relaykamome_thirtytwo = 809...856
+    static let midori_thirtyfour = 856...869
+    static let relaykamome_thirtysix = 869...916
+    static let midori_HTB_thirtyeight = 916...928
+    static let relaykamome_forty = 928...976
+    static let midori_HTB_fortytwo = 976...1002
+    static let relaykamome_fortyfour = 1002...1033
+    static let midori_fortysix = 1033...1070
+    static let relaykamome_fortyeight = 1070...1095
+    static let sanjuuroku = 1095...1104
+    static let midori_fifty = 1104...1128
+    static let relaykamome_fiftytwo = 1128...1155
+    static let midori_fiftyfour = 1155...1191
+    static let relaykamome_fiftysix = 1191...1218
+    static let midori_fiftyeight = 1218...1257
+    static let midori_sixty = 1257...1291
+    static let relaykamome_sixtytwo = 1291...1309
+    static let relaykamome_sixtyfour = 1309...1363
 }
 
 struct NagasakiTimeData {
